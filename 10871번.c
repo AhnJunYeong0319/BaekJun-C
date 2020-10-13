580c28f6ec6cb1df20249acccf927cafe16d0b1b
안준영 (JunYeong Ahn) <hewas321@gmail.com>
2020-10-14 01:11:40 +0900

    Add files via upload

---
+++ 10871번.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+main() {
+	int N, X;
+	scanf("%d %d", &N, &X);
+
+	int A[10001] = { 0 };
+
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &A[i]);
+	}
+	for (int i = 0; i < N; i++) {
+		if (A[i] < X) {
+			printf("%d ", A[i]);
+		}
+	}
+}
---
+++ 11021번.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+main() {
+	int T, A, B;
+	scanf("%d", &T);
+	for (int i = 0; i < T; i++) {
+		scanf("%d %d", &A, &B);
+		printf("Case #%d: %d\n", i+1, A + B);
+	}
+}
